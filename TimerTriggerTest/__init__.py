5fc39910443d3915d1a0d6358f5b580a7c87afcb
thesebastiaan <sebastiaan@thunderranch.be>
2023-03-30 15:49:41 +0200

    Var test

--- TimerTriggerTest/__init__.py
+++ TimerTriggerTest/__init__.py
@@ -11,5 +11,6 @@ def main(mytimer: func.TimerRequest) -> None:
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
+        logging.info(EVENT_HUB_NAME)
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
