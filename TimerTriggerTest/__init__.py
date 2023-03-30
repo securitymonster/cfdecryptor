da09bbb95f589d2923bcb7cd6204349eb946c837
thesebastiaan <sebastiaan@thunderranch.be>
2023-03-30 16:00:53 +0200

    Logging update

--- TimerTriggerTest/__init__.py
+++ TimerTriggerTest/__init__.py
@@ -1,10 +1,22 @@
 # Simple test
+from cfdecryptlog import EVENT_HUB_NAME
 import datetime
 import logging
 
 import azure.functions as func
 
-from cfdecryptlog import EVENT_HUB_NAME
+event_hub_name = os.environ.get('EVENT_HUB_NAME')
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+        logging.info(f'The value of SOME_VALUE is {event_hub_name}')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
 
 def main(mytimer: func.TimerRequest) -> None:
